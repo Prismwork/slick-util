8e8804700782cb0d75e27954326c1341a04eb3ef
Flamarine <William27060827@hotmail.com>
2024-03-09 16:50:45 +0800

    Comment out unused libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,17 +17,19 @@ dependencies {
     implementation("org.lwjgl.lwjgl:lwjgl:${lwjglVersion}")
     runtimeOnly("org.lwjgl.lwjgl:lwjgl-platform:${lwjglVersion}")
 
-    val log4jVersion = "2.20.0"
-    implementation("org.apache.logging.log4j:log4j-core:${log4jVersion}")
-    implementation("org.apache.logging.log4j:log4j-api:${log4jVersion}")
+    // val log4jVersion = "2.20.0"
+    // implementation("org.apache.logging.log4j:log4j-core:${log4jVersion}")
+    // implementation("org.apache.logging.log4j:log4j-api:${log4jVersion}")
 
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    // testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    // testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
 }
 
+/*
 tasks.test {
     useJUnitPlatform()
 }
+*/
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
